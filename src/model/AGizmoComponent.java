c23e7eba57d50a00b28b9a91f2c3a645ef40a915
benny_wan <benny_wan95@yahoo.co.uk>
2016-02-09 15:17:42 +0000

    Made a start on AGizmoComponent Abstract class

--- src/model/AGizmoComponent.java
+++ src/model/AGizmoComponent.java
@@ -1,5 +1,17 @@
 package model;
 
-abstract public class AGizmoComponent {
+import java.awt.Color;
 
+abstract public class AGizmoComponent {
+	/** The x-coordinate of the component. Along with y-coordinate, this determines the position of a Gizmo component
+	 * on the board. Exact  **/
+	private double xpos;
+	private double ypos;
+	private Color colour;
+	
+	public AGizmoComponent(double x, double y, Color color) {
+		xpos = x; // Centre coordinates
+		ypos = y;
+		this.colour = color;
+	}
 }
