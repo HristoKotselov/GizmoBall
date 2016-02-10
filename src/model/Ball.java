09a06015222699bf668227edb4fc3d0616bb402a
Hristo Kotselov <h.kotselov1314@gmail.com>
2016-02-10 01:34:17 +0000

    most of the useful code from the Demo for collisions and some other configurations

--- src/model/Ball.java
+++ src/model/Ball.java
@@ -1,5 +1,78 @@
 package model;
 
-public class Ball extends AGizmoComponent {
+import java.awt.Color;
 
+import physics.Angle;
+import physics.Circle;
+import physics.Vect;
+
+public class Ball extends AGizmoComponent {
+	
+	private Vect velocity;
+	private double radius;
+	private double xpos; // most likely from abstract class and will be removed from here
+	private double ypos; // same as above
+	private Color colour;
+
+	private boolean stopped;
+
+	// x, y coordinates and x,y velocity
+	public Ball(double x, double y, Angle theta, double yv) {
+		xpos = x; // Centre coordinates
+		ypos = y;
+		colour = Color.BLUE;
+		velocity = new Vect(theta, yv);
+		radius = 5;
+		stopped = false;
+	}
+
+	public Vect getVelo() {
+		return velocity;
+	}
+
+	public void setVelo(Vect v) {
+		velocity = v;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public Circle getCircle() {
+		return new Circle(xpos, ypos, radius);
+
+	}
+
+	// Ball specific methods that deal with double precision.
+	public double getExactX() {
+		return xpos;
+	}
+
+	public double getExactY() {
+		return ypos;
+	}
+
+	public void setExactX(double x) {
+		xpos = x;
+	}
+
+	public void setExactY(double y) {
+		ypos = y;
+	}
+
+	public void stop() {
+		stopped = true;
+	}
+
+	public void start() {
+		stopped = false;
+	}
+
+	public boolean stopped() {
+		return stopped;
+	}
+
+	public Color getColour() {
+		return colour;
+	}
 }
