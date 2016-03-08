9f2d8cd07705372fb278114e7c7d637cfdb9f83f
Hristo Kotselov <h.kotselov1314@gmail.com>
2016-03-08 18:07:19 +0000

    implemented the models for the bumpers

--- src/model/CircularBumper.java
+++ src/model/CircularBumper.java
@@ -4,30 +4,39 @@ import java.awt.Color;
 import java.awt.Shape;
 
 import physics.Angle;
+import physics.Circle;
 
 public class CircularBumper extends AStatueGizmo {
+	
+	private int radius;
 
 	public CircularBumper(String name, int grid_tile_x, int grid_tile_y, Color color) {
 		super(name, grid_tile_x * MainEngine.L, grid_tile_y * MainEngine.L, color);
-		// TODO Auto-generated constructor stub
+		radius = 10;
+		Circle circle = new Circle(grid_tile_x * MainEngine.L,grid_tile_y * MainEngine.L, radius);
+		circleSet.add(circle);
 	}
 
 	@Override
 	protected void setupDrawingShape() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	protected void setupCircles() {
-		// TODO Auto-generated method stub
-
+		Circle circle = new Circle(getX() + 10, getY() + 10, 10);
+		circleSet.add(circle);
 	}
 
 	@Override
 	public void triggerAction() {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public double getRadius() {
+		return radius;
 	}
 
 }
