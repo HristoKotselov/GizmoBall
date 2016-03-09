44fcf5421045dec4c6cb0d70e4dbaf40a38bc192
Sam <sammcgregor13@gmail.com>
2016-03-09 16:43:16 +0000

    Moved the build mode placing code from mouseClicked() to mousePressed(). This means the gizmos are placed when you click rather than when you click and release. Before, if you clicked, moved the mouse, then released, nothing happened.

--- src/controller/BuildModeMouseListener.java
+++ src/controller/BuildModeMouseListener.java
@@ -30,6 +30,51 @@ public class BuildModeMouseListener implements MouseListener {
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		//System.out.println(e.getX() / 20 + ", " + e.getY() / 20);
+/*
+		int x = e.getX() / 20;
+		int y = e.getY() / 20;
+		String selectedType = "";
+		for (Enumeration<AbstractButton> buttons = bm.getGizmoSelector().getElements(); buttons.hasMoreElements();) {
+			AbstractButton button = buttons.nextElement();
+
+			if (button.isSelected()) {
+				selectedType = button.getText();
+			}
+		}
+		AGizmoComponent g = null;
+
+		switch (selectedType) {
+		case "square":
+			g = new SquareBumper("s" + x + y, x, y, Color.GREEN);
+
+			break;
+		case "triangle":
+			g = new TriangularBumper("t" + x + y, x, y, Color.red);
+
+			break;
+		case "circle":
+			g = new CircularBumper("c" + x + y, x, y, Color.blue);
+
+			break;
+		case "left flipper":
+			g = new Flipper("f" + x + y, x, y, Color.ORANGE, true);
+
+			break;
+		case "right flipper":
+			g = new Flipper("f" + x + y, x, y, Color.ORANGE, false);
+
+			break;
+		default:
+			System.out.println(
+					"Not recognised or not Implimented, make sure the gizmo is in the BuildModeMouseListener.mouseClicked() case statement.");
+			return;
+		}
+
+		m.addGizmo(g);*/
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
 
 		int x = e.getX() / 20;
 		int y = e.getY() / 20;
@@ -71,11 +116,6 @@ public class BuildModeMouseListener implements MouseListener {
 		}
 
 		m.addGizmo(g);
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-
 	}
 
 	@Override
