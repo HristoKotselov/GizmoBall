10931aa3b27845b7df07ab0d73f2f4f6c45dadaf
GregRoots1994 <greg.roots@yahoo.co.uk>
2016-03-14 19:10:19 +0000

    save file listener done

--- src/controller/SaveFileListener.java
+++ src/controller/SaveFileListener.java
@@ -2,24 +2,28 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import model.IMainEngine;
 import model.ISaveDataEngine;
+import view.IGameWindow;
 
 public class SaveFileListener implements ActionListener {
-	private IMainEngine model;
+	private IGameWindow window;
 	private ISaveDataEngine dataEngine;
 
-	public SaveFileListener(IMainEngine m) {
-		model = m;
+	public SaveFileListener(IGameWindow w, ISaveDataEngine s) {
+		window = w;
+		dataEngine = s;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String actionCmd = e.getActionCommand();
-		if (actionCmd.equals("save")) {
-			//TODO save file
-		}
 
+		if (actionCmd.equals("save")) {
+			String path = window.getFile();
+
+			if (path != null) {
+				dataEngine.saveFile(path);
+			}
+		}
 	}
 }
