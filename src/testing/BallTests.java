8ac1d3c9e723790f55ca0d7d79ab9c826f124375
GregRoots1994 <greg.roots@yahoo.co.uk>
2016-03-19 20:48:34 +0000

    updated tests

--- src/testing/BallTests.java
+++ src/testing/BallTests.java
@@ -1,12 +1,14 @@
 package testing;
 
 import static org.junit.Assert.*;
-
+import java.awt.Shape;
 import java.awt.Color;
-
+import java.util.Set;
+import physics.Vect;
 import org.junit.Test;
 import model.*;
 import physics.Angle;
+import physics.Circle;
 public class BallTests {
 	Angle test1 = new Angle(25);
 	Angle test2 = new Angle(75);
@@ -31,5 +33,38 @@ public class BallTests {
 		assertTrue(b.getRadius() == 5);
 		assertTrue(c.getRadius() == 5);
 	}
+	
+	@Test
+	public void circleSetTest(){
+		
+		Set<Circle> testCirc = a.getCircles();
+		assertNotNull(testCirc);
+	}
+	
+	@Test
+	public void testGetShape(){
+		Shape s;
+		s = a.getDrawingShape();
+		assertNotNull(s);
+	}
+	
+	@Test
+	public void resetTest(){
+		Vect testing = new Vect(Angle.DEG_270, 50);
+		a.setVelo(testing);
+		a.move(1,2);
+		a.reset();
+		assertFalse(a.getVelo() == testing);
+	}
+	
+	@Test
+	public void circleNotNull(){
+		Circle c = a.getCircle();
+		Circle d = a.getCircle();
+		assertEquals(c,d);
+		assertNotNull(c);
+		assertNotNull(d);
+		
+		}
 
 }
