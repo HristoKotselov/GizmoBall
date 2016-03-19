f075880dfe40af75c3c74a000b93a4ec2945672c
GregRoots1994 <greg.roots@yahoo.co.uk>
2016-03-19 20:34:21 +0000

    more tests added, more to come

--- src/testing/BallTests.java
+++ src/testing/BallTests.java
@@ -12,15 +12,24 @@ public class BallTests {
 	Angle test2 = new Angle(75);
 	
 	
-	// TODO Ball constructor updated, so need to change those 2.0s & 2.5s into int
-	/*
-	Ball a = new Ball("A1", Color.red, 2.0, 2.0, test1, 3.0);
-	Ball b = new Ball("B1", Color.blue, 2.5, 2.5, test2, 3.5);
-	Ball c = new Ball("C1", Color.red, 2.0, 2.0, test1, 3.0);
-	*/
+	
+	
+	Ball a = new Ball("A1", Color.red, 2, 2, test1, 3.0);
+	Ball b = new Ball("B1", Color.blue, 3, 3, test2, 3.5);
+	Ball c = new Ball("C1", Color.red, 2, 2, test1, 3.0);
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRotation() {
+		assertTrue(a.rotate(11));
+		assertTrue(b.rotate(12));
+		assertTrue(c.rotate(13));
+	}
+	
+	@Test
+	public void testGetRadius(){
+		assertTrue(a.getRadius() == 5);
+		assertTrue(b.getRadius() == 5);
+		assertTrue(c.getRadius() == 5);
 	}
 
 }
